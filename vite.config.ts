595e984c625d795717803f55b0fc59e9a3e781a0
root <steven-jocz@hotmail.com>
2023-07-16 22:40:57 -0500

    vite-deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base : 'https://stevenjocz.github.io/vite-deploy'
+  base : 'https://stevenjocz.github.io/HistorialClinicointerfaz/'
 })
